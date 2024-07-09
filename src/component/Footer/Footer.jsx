fbd4a54b6038f62ba256654819081c4a912e2221
Haseeb <zed.h8050@gmail.com>
2024-07-10 02:54:00 +0500

    footer links has been updated

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -8,8 +8,10 @@ import {
   FooterPintrest,
   FooterTwitter,
 } from "../Icons";
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+  const navigate = useNavigate();
   return (
     <div className="pt-10 bg-[#ABABAB] px-4 md:px-10 lg:px-24 font-inter">
       <div className="pb-2 border-b">
@@ -34,10 +36,24 @@ const Footer = () => {
         </div>
         <div className="flex flex-col text-white lg:text-lg gap-2 py-4 flex-1 border-t mt-10 md:border-l md:border-t-0 px-10">
           <h6 className="font-semibold">Explore </h6>
-          <span>About Us</span>
-          <span>Services</span>
-          <span>Our Team</span>
-          <span>Project Gallery</span>
+          <span className="cursor-pointer" onClick={() => navigate("/AboutUs")}>
+            About Us
+          </span>
+          <span
+            className="cursor-pointer"
+            onClick={() => navigate("/ServicesPage")}
+          >
+            Services
+          </span>
+          <span className="cursor-pointer" onClick={() => navigate("/OurTeam")}>
+            Our Team
+          </span>
+          <span
+            className="cursor-pointer"
+            onClick={() => navigate("/ProjectGalleryPage")}
+          >
+            Project Gallery
+          </span>
         </div>
         <div className="text-white flex flex-col gap-4 px-10 py-4 border-t md:border-l mt-10 md:border-t-0">
           <div className="flex items-center gap-2">
